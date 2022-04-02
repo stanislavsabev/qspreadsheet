c7a607a84321ffe9dd208c2b1a824afcfbbdef39
stanislavsabev <bezraboten.34@gmail.com>
2022-04-02 21:52:03 +0200

    Add conftest.py in tests/.

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+"""Pytest fixtures."""
+
+import pytest
+
+from qspreadsheet import qt
+
+
+@pytest.fixture(scope='session')
+def qapp():
+    """Yields instance of QApplication."""
+    app = qt.QApplication([])
+    yield app
+    del app
