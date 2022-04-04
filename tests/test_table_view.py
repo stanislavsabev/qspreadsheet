77ec7dc0dfa01f6ca64ddc02b6fbdf83ac23672f
stanislavsabev <bezraboten.34@gmail.com>
2022-04-04 03:57:49 +0200

    GIT RERevert "Group some tests to 'serial', to pass in ubuntu."
    This reverts commit fb66467545b62e1207b8eec362e6d97ac33318af.

--- tests/test_table_view.py
+++ tests/test_table_view.py
@@ -1,11 +1,9 @@
 """Test for the TableView object."""
 
-import pytest
 from qspreadsheet import table_view
 from tests import util
 
 
-@pytest.mark.serial
 def test_create(qapp):
     """TableView can be created."""
     util.test_create(table_view.TableView)
