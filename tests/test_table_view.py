e523303c589fc5f92d1cd39781703824f59d9ee4
stanislavsabev <bezraboten.34@gmail.com>
2022-04-04 02:18:53 +0200

    Delete all qt tests.

--- tests/test_table_view.py
+++
@@ -1,9 +0,0 @@
-"""Test for the TableView object."""
-
-from qspreadsheet import table_view
-from tests import util
-
-
-def test_create(qapp):
-    """TableView can be created."""
-    util.test_create(table_view.TableView)
