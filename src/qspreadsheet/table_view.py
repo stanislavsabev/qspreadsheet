20511c46a77a264501ebcddf2c5ed524982dc6a6
stanislavsabev <bezraboten.34@gmail.com>
2022-04-13 23:44:30 +0200

    Adding two methods to TableView to test the badge.

--- src/qspreadsheet/table_view.py
+++ src/qspreadsheet/table_view.py
@@ -13,3 +13,36 @@ class TableView(qt.QTableView):
             parent: A QWidget, optional, to be assigned as parent.
         """
         super().__init__(parent)
+
+    def contextMenuEvent(self, event: qt.QContextMenuEvent) -> None:
+        """Handles right-clicking on a cell.
+
+        When subclassing, you probably want to overrite
+        `make_cell_context_menu`, not this function.
+
+        Args:
+            event: QContextMenuEvent to handle.
+        """
+        row_ndx = self.rowAt(event.y())
+        col_ndx = self.columnAt(event.x())
+        if row_ndx < 0 or col_ndx < 0:
+            return
+
+        menu = self.make_cell_context_menu(row_ndx, col_ndx)
+        pos = self.mapToGlobal(event.pos())
+        menu_pos = qt.QPoint(pos.x() + 20, pos.y() + menu.height() + 20)
+        menu.exec_(menu_pos)
+
+    def make_cell_context_menu(self, row_ndx: int, col_ndx: int) -> qt.QMenu:
+        """Create cell context menu.
+
+        Args:
+            row_ndx: An int.
+            col_ndx: An int.
+
+        Returns:
+            A QMenu.
+        """
+
+        menu = qt.QMenu('context menu', self)
+        return menu
