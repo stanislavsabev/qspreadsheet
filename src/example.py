13749cf7d8379c90446b90e4961707a72a9cefbc
stanislavsabev <bezraboten.34@gmail.com>
2023-05-06 19:46:46 +0200

    Add example

--- src/example.py
+++ src/example.py
@@ -3,6 +3,8 @@
 import sys
 from typing import Callable, Optional
 
+import pandas as pd
+
 from qspreadsheet import qt
 from qspreadsheet import table_widget
 
@@ -10,31 +12,26 @@ from qspreadsheet import table_widget
 class MainWindow(qt.QMainWindow):
     """Main GUI Window."""
 
-    def __init__(self, parent: Optional[qt.QWidget] = ...):
+    def __init__(self, parent: Optional[qt.QWidget] = None):
         """Create MainWindow object.
 
         Args:
             parent (qt.QWidget, optional): Window's parent. Defaults to None.
         """
         super(MainWindow, self).__init__(parent)
-
-        self._org = 'qt'
-        self._app = 'qspreadsheet'
+        self._window_settings = qt.QSettings(qt.QSettings.IniFormat,
+                                qt.QSettings.UserScope,
+                                'qt',
+                                'qspreadsheet')
+        self._settings_group = self.__class__.__name__
         self._default_size = qt.QSize(800, 480)
-
-        central_w = qt.QWidget(self)
-        central_l = qt.QVBoxLayout(central_w)
-        central_w.setLayout(central_l)
-        table_w = table_widget.TableWidget()
-
-        table_l = qt.QHBoxLayout()
-        table_l.addWidget(table_w)
-        table_w.setParent(table_l)
-        central_l.addLayout(table_l)
-
-        self.setCentralWidget(central_w)
+        central_widget = qt.QWidget(self)
+        central_layout = qt.QVBoxLayout(central_widget)
+        central_widget.setLayout(central_layout)
+        
+        self.setCentralWidget(central_widget)
         self.setWindowTitle('qspreadsheet')
-        self.apply_settings(self.load_settings)
+        self.load_settings()
 
     def closeEvent(self, event: qt.QCloseEvent):
         """Handles the close event for this window
@@ -42,46 +39,57 @@ class MainWindow(qt.QMainWindow):
         Args:
             event (qt.QCloseEvent): close event.
         """
-        self.apply_settings(self.save_settings)
+        self.save_settings()
         event.accept()
 
-    def apply_settings(self, func: Callable[[qt.QSettings], None]):
-        """Apply window settings ot own group and close it.
-
-        Args:
-            func (Callable[[qt.QSettings], None]): Settings function.
-        """
-        settings = qt.QSettings(qt.QSettings.IniFormat,
-                                qt.QSettings.UserScope,
-                                self._org,
-                                self._app)
-        settings.beginGroup('MainWindow')
-        func(settings)
-        settings.endGroup()
-
-    def save_settings(self, settings: qt.QSettings):
+    def save_settings(self):
         """Save window settings like size and position.
 
         Args:
             settings (qt.QSettings): Window settings.
         """
+        settings = self._window_settings
+        settings.beginGroup(self._settings_group)
         settings.setValue('size', self.size())
         settings.setValue('pos', self.pos())
+        settings.endGroup()
 
-    def load_settings(self, settings: qt.QSettings):
+    def load_settings(self):
         """Load window settings like size and position.
 
         Args:
             settings (qt.QSettings): Window settings.
         """
+        settings = self._window_settings
+        settings.beginGroup(self._settings_group)
         self.resize(qt.QSize(settings.value('size', self._default_size)))  # type: ignore
         self.move(qt.QPoint(settings.value('pos', qt.QPoint(200, 200))))  # type: ignore
+        settings.endGroup()
+
+    def add_table(self, tbl_widget: qt.QWidget):
+        central_layout = self.centralWidget().layout()
+        tbl_layout = qt.QHBoxLayout()
+        tbl_layout.addWidget(tbl_widget)
+        tbl_widget.setParent(tbl_layout)
+        central_layout.addLayout(tbl_layout)
+
+
+def load_df():
+    area = pd.Series({0 : 423967, 1: 695662, 2: 141297, 3: 170312, 4: 149995})
+    pop = pd.Series({0 : 38332521, 1: 26448193, 2: 19651127, 3: 19552860, 4: 12882135})
+    states = ['California', 'Texas', 'New York', 'Florida', 'Illinois']
+    data = {'states': states, 'area': area, 'pop': pop}
+    df = pd.DataFrame(data=data)
+    df['overcrowded'] = (df['pop'] / df['area'] > 100)
+    return df
 
 
 def main():
     """Entry point for this script."""
     app = qt.QApplication(sys.argv)
+    tbl_widget = table_widget.TableWidget(data=load_df())
     window = MainWindow()
+    window.add_table(tbl_widget)
     window.show()
     sys.exit(app.exec_())
 
